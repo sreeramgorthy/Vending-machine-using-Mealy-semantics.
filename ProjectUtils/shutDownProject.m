c4196d598b54dabceaad09ea69a9e7ae8eda5b9d
sreeramgorthy <sreeramgorthy@gmail.com>
2020-12-15 15:04:11 +0530

    Designed basic model.

---
+++ ProjectUtils/shutDownProject.m
@@ -0,0 +1,4 @@
+% This program executes during closing of the project.
+clearvars
+clc
+bdclose all
---
+++ ProjectUtils/startUpProject.m
@@ -0,0 +1,4 @@
+% This program executes during the start of the project.
+clearvars
+clc
+bdclose all
